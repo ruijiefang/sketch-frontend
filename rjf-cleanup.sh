a13b6cc8cc7e7492226dace07b7c83166a8c7557
Ruijie Fang <ruijief@princeton.edu>
2020-12-05 23:54:27 -0500

    build scripts by rjf

---
+++ rjf-cleanup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -x
+rm -rf ../sketch-1.7.6
+rm -rf ../*.tar
+
--- rjf-compile.sh
+++ rjf-compile.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 make light-distr
+cp ./sketchCustom ../sketch-1.7.6/sketch-frontend
+cp ~/Projects/cos516project/helloworld.sk ../sketch-1.7.6/sketch-frontend 
+cd ../sketch-1.7.6/sketch-frontend
