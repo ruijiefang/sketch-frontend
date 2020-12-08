0e6d40b5174437249afa0d6ae8b82c29e709c73b
Ruijie Fang <ruijief@princeton.edu>
2020-12-08 02:08:01 -0500

    debugging

--- src/main/java/sketch/compiler/main/seq/SequentialSketchMainCustom.java
+++ src/main/java/sketch/compiler/main/seq/SequentialSketchMainCustom.java
@@ -413,7 +413,10 @@ public class SequentialSketchMainCustom {
         grammarProg = (Program) grammarProg.accept(new MemoizeTemporaries(k));
         /* Append components to the grammar file. */
         grammarProg.accept(new AppendFunctions(componentFuncs, componentsProg.getPackages().get(0).getName()));
-
+        System.out.println("Grammar Program: Before inserting harness ------------------");
+        System.out.println(grammarProg.toString());
+        System.out.println("----------------------- Inserting harness ------------------");
+        ArrayList<Function> harnesses = new ArrayList<>();
         for (ComponentArguments.ArgInComponent comp : componentArgs) {
             ArrayList<String> intArgs = new ArrayList<>(), bitArgs = new ArrayList<>();
             for (Parameter par : comp.componentArgs) {
@@ -438,78 +441,16 @@ public class SequentialSketchMainCustom {
             } else {
                 stmts.add(makeAssert(grammarProg.getOrigin(), GNAME, comp, "vars", "bit_vars", k));
             }
-            Function.FunctionCreator harness = makeHarness(componentProgramNode)
+            Function harness = makeHarness(componentProgramNode)
                     .name(MANGLE_HARNESS + comp.componentName)
                     .returnType(comp.rty)
-                    .body(new StmtBlock(grammarProg.getOrigin(), stmts));
-
+                    .body(new StmtBlock(grammarProg.getOrigin(), stmts)).create();
+            harnesses.add(harness);
         }
+        System.out.println("Now actually adding all the harnesses.");
+        grammarProg = (Program) grammarProg.accept(new AppendFunctions(harnesses, grammarPkg));
+        System.out.println("Grammar Program: Final form: ----------------------------------------------");
+        System.out.println(grammarProg.toString());
     }
 
-    public static void go(String[] args) {
-        System.out.println("CUSTOM SKETCH version " +
-                PlatformLocalization.getLocalization().version);
-        long beg = System.currentTimeMillis();
-        ErrorHandling.checkJavaVersion(1, 6);
-        // TODO -- change class names so this is clear
-        final SequentialSketchMain sketchmain = new SequentialSketchMain(args);
-        PlatformLocalization.getLocalization().setTempDirs();
-        int exitCode = 0;
-        try {
-            SketchOptions options = SketchOptions.getSingleton();
-            if (options.feOpts.timeout > 0) {
-                ExecutorService executor = Executors.newSingleThreadExecutor();
-                Future<?> f = executor.submit(sketchmain);
-                try {
-                    f.get((long) options.feOpts.timeout, TimeUnit.MINUTES);
-                } catch (TimeoutException e) {
-                    System.out.println("Sketch front-end timed out");
-                    exitCode = 1;
-                } catch (ExecutionException e) {
-                    ErrorHandling.handleErr(e);
-                    exitCode = 1;
-                } catch (InterruptedException e) {
-                    ErrorHandling.handleErr(e);
-                    exitCode = 1;
-                } finally {
-                    executor.shutdown();
-                }
-            } else { // normal run
-                // System.out.println("Running");
-                sketchmain.run();
-                // System.out.println("End run");
-            }
-        } catch (SketchException e) {
-            e.print();
-            if (isTest) {
-                throw e;
-            } else {
-                // e.printStackTrace();
-                exitCode = 1;
-            }
-        } catch (java.lang.Error e) {
-            ErrorHandling.handleErr(e);
-            // necessary for unit tests, etc.
-            if (isTest) {
-                throw e;
-            } else {
-                exitCode = 1;
-            }
-        } catch (RuntimeException e) {
-            ErrorHandling.handleErr(e);
-            if (isTest) {
-                throw e;
-            } else {
-                if (sketchmain.options.debugOpts.verbosity > 3) {
-                    e.printStackTrace();
-                }
-                exitCode = 1;
-            }
-        } finally {
-            System.out.println("Total time = " + (System.currentTimeMillis() - beg));
-        }
-        if (exitCode != 0) {
-            System.exit(exitCode);
-        }
-    }
 }
